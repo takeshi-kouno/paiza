8466f2694ef2c73202dc0a4c0dc385674fdc5143
kounotakeshi <t.kouno.1018@gmail.com>
2022-01-11 17:38:04 +0900

    feature

--- 標準出力/index.rb
+++ 標準出力/index.rb
@@ -2,10 +2,9 @@ n = gets.to_i
 
 members = Array.new(n)
 
-n.times { |i| members[i] = gets.split(' ')}
+n.times { |i| members[i] = gets.split(" ")}
 
 members.each do |member|
-    name = member [0]
-    age = member[1].to_i
-    puts name + ' ' + (age + 1).to_s
+    name, age = member [0], member[1].to_i
+    puts "#{name}#{" "}#{age + 1}"
 end
