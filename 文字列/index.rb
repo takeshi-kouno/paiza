5129489c8a01e97433c668712cdfe62a6220cc57
kounotakeshi <t.kouno.1018@gmail.com>
2022-02-05 17:13:39 +0900

    teature

--- 文字列/index.rb
+++ 文字列/index.rb
@@ -1,59 +1,42 @@
-# 入力例2
-# 2
-# 15:59 0 1
-# 23:20 1 0
-
-
-# 入力を受け取る
-# 工事時間のデータを格納する配列を作る
+#入力受取
 N = gets.to_i
 
-schedule = []
+#工事の時刻を受け取って配列にする
+schedules = []
 
 N.times{
-  info = gets.split(" ")
-  schedule.push(info)
-  startTime = info[0].split(":")
-  workTime = info[1..2]
+  schedule = gets.split(" ")
+  startTime = schedule[0].split(":").map(&:to_i)
+  workTime = schedule[1..2].map(&:to_i)
 
-  startTime: {hour: startTime[0], min: startTime[1]}.to_i
-  workTime: {hour: workTime[0], min: workTime[1]}.to_i
+  schedule = {
+    startTime: {hour: startTime[0], min: startTime[1]},
+    workTime: {hour: workTime[0], min: workTime[1]}
+  }
+
+  schedules.push(schedule)
 }
 
-# 終了時間を計算したデータを格納する配列を作る
-
-#answer = []
-
-schedule.each{
-# "15:59"
-  #startTime = s[0] 
-# ["15","59"]
-  #ss = startTime.split(":")
-
-  #startHour = ss[0].to_i
-  #startMin = ss[1].to_i
-  #workHour = s[1].to_i
-  #workMin = s[2].to_i
-# ------いじる
+endtimeList = []
+#計算する
+schedules.each{|schedule|
+  startTime = schedule[:startTime]
+  workTime = schedule[:workTime]
 
 totalHour = startTime[:hour] + workTime[:hour]
 totalMin = startTime[:min] + workTime[:min]
 
-# ---- 
-
+#繰り上げ計算
 if totalMin >= 60
   totaltHour + 1
   totalMin - 60
 end
 
-# -----
-
 if totalHour >= 24
   totalHour -= 24
 end
 
-# ----
-
+#一桁の場合０を追加
 if totalHour.to_s.length == 1
   endHour = "0#{totalHour.to_s}"
 else
@@ -66,14 +49,11 @@ else
   endMin = totalMin.to_s
 end
 
-
 endTime = "#{endHour}:#{endMin}"
-
-#answer.puth(answerTime)
+endtimeList.push(endTime)
 }
 
-# 出力する
-
-endTime.each{|answer|
+#ループして出力
+endtimeList.each{|answer|
   puts answer
 }
