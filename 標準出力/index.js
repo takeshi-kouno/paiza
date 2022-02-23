58567b0b5c94f74726a67619beea910603e2a673
kounotakeshi <t.kouno.1018@gmail.com>
2022-02-23 16:38:37 +0900

    feature

--- 標準出力/index.js
+++ 標準出力/index.js
@@ -1,13 +1,14 @@
 const input = require("fs").readFileSync("/dev/stdin", "utf8").trim();
 
-const info = input.split("\n");
+const lines = input.split("\n");
 
-const membersList = info.slice(1);
+const [N, ...memberInfo] = lines;
+const memberInfonew = memberInfo.map((member) => {
+  var [name, age] = member.split(" ");
+  age = Number(age);
+  return { name, age };
+});
 
-const N = parseFloat(info[0]);
-
-membersList.map((list) => {
-  const memberInfo = list.split(" ");
-  const member = { name: memberInfo[0], age: memberInfo[1] };
-  console.log(member["name"], parseFloat(member["age"]) + 1);
+memberInfonew.forEach((member) => {
+  console.log(member.name, member.age + 1);
 });
